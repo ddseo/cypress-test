4aef8bf40464705d39f6f595ea3e1d46d9fda1bc
Derek Seo <derek.seo@autodesk.com>
2018-11-02 10:55:58 -0400

    Commenting and logging version

--- cypress/integration/should_not.js
+++ cypress/integration/should_not.js
@@ -1,8 +1,10 @@
+// Page doesn't matter as long as it doesn't make Cypress complain about cross-reference
 const url = 'http://www.test.bbc.co.uk/sport/beta/live/football/23031537';
 
 describe('Test empty selector', () => {
     it('passes should not test on an empty selector', () => {
        cy.visit(url);
+       cy.log(`Cypress Version: ${Cypress.version}`);
        cy.get('.nothingOnAnyRealPageShouldMatchThisSelector')
            .should('not.be.disabled');
        cy.get('thisShouldAlsoReturnAnEmptySelector')
